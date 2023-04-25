874cf60c6576b5cd25a5e545b4d8c53fb96b8fd2
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-04-25 23:11:18 +0300

    project

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
 const apiUrl = "http://localhost:3000/Data"
 //the apiUrl contains the api containing the db.json data
+
+// Fetch the product data from the API
 fetch(apiUrl)
-.then(res=>res.json())
-.then(data=>{
+  .then(res => res.json())
+  .then(data => {
     const productsDiv = document.getElementById('products');
-    //This gets the div container to store all the products
+    // This gets the div container to store all the products
     let html = '';
-    data.forEach(product => {
-        html += `
-        <div>
-          <h2>${product.productName}</h2>
-          <p>Category: ${product.category}</p>
-          <p>Price: ${product.price}</p>
-          <img src="${product.image}" alt="${product.productName}">
-        </div>
-      `;
+    // Store the product data in an array for filtering
+    let products = data;
+
+    // Display all products initially
+    displayProducts(products);
+
+    // Add event listener for the search button
+    const searchBtn = document.getElementById('search');
+    searchBtn.addEventListener('click', () => {
+      const searchTerm = document.getElementById('search-input').value.toLowerCase();
+      // Filter products based on search term
+      products = data.filter(product =>
+        product.productName.toLowerCase().includes(searchTerm)
+      );
+      // Display filtered products
+      displayProducts(products);
     });
-    productsDiv.innerHTML = html;
-})
+
+    // Add event listener for the category buttons
+    const categoryBtns = document.querySelectorAll('.button-value');
+    categoryBtns.forEach(btn => {
+      btn.addEventListener('click', () => {
+        const category = btn.textContent;
+        // Filter products based on category
+        products = data.filter(product =>
+          product.category === category
+        );
+        // Display filtered products
+        displayProducts(products);
+      });
+    });
+
+    // Function to display products
+    function displayProducts(products) {
+      html = '';
+      products.forEach(product => {
+        html += `
+          <div>
+            <h2>${product.productName}</h2>
+            <p>Category: ${product.category}</p>
+            <p>Price: ${product.price}</p>
+            <img src="${product.image}" alt="${product.productName}">
+          </div>
+        `;
+      });
+      productsDiv.innerHTML = html;
+    }
+  });
+
---
+++ note.md
@@ -0,0 +1,20 @@
+const apiUrl = "http://localhost:3000/Data"
+//the apiUrl contains the api containing the db.json data
+fetch(apiUrl)
+.then(res=>res.json())
+.then(data=>{
+    const productsDiv = document.getElementById('products');
+    //This gets the div container to store all the products
+    let html = '';
+    data.forEach(product => {
+        html += `
+        <div>
+          <h2>${product.productName}</h2>
+          <p>Category: ${product.category}</p>
+          <p>Price: ${product.price}</p>
+          <img src="${product.image}" alt="${product.productName}">
+        </div>
+      `;
+    });
+    productsDiv.innerHTML = html;
+});
